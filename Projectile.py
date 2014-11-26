630198c8d465387232b3353e875781358450ad09
Connor Lirot <Connor@Connor-Lirots-MacBook-Pro.local>
2014-11-26 02:22:09 -0600

    changed projectile to use frames instead of seconds for live time

--- Projectile.py
+++ Projectile.py
@@ -4,14 +4,12 @@ from Entity import *
 class Projectile (Entity) :
 	def __init__(self,x=0,y=0,width=0,height=0, **images) :
 		Entity.__init__ (self,x,y,width,height,**images)
-		self.launch_time = None
-		self.time_to_live = 1000
+		self.frames_to_live = 60
 
 	#launch the projectile
 	#in most cases, this should be called immediately after spawning into the game
 	def launch (self, velocity) :
 		self.velocity = velocity
-		self.launch_time = pygame.time.get_ticks ()
 
 	#how much knockback does this projectile transfer to its target?
 	def get_knockback (self) :
@@ -19,16 +17,15 @@ class Projectile (Entity) :
 	def set_knockback (self, knockback) :
 		self.knockback = knockback
 
-	#how much time (ms) does this projectile have before it despawns?
+	#how many screen updates does this projectile have before it despawns?
 	def get_time_to_live (self) :
-		return self.time_to_live
-	def set_time_to_live (self, time_to_live) :
-		self.time_to_live = time_to_live
+		return self.frames_to_live
+	def set_time_to_live (self, frames_to_live) :
+		self.frames_to_live = frames_to_live
 
 	def update (self) :
-		current_time = pygame.time.get_ticks ()
-		alive_time = current_time - self.launch_time
-		if alive_time > self.time_to_live :
+		self.frames_to_live -= 1
+		if self.frames_to_live < 0 :
 			self.delegate.despawn_entity (self)
 
 		entities = self.delegate.get_all_entities ()
