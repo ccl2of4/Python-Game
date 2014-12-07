1f80acc2ce011ddd004ec5ae876955050c293dc7
Connor <drconnoramerica@utexas.edu>
2014-12-06 20:19:57 -0600

    optimized statusdisplay

--- statusdisplay.py
+++ statusdisplay.py
@@ -18,20 +18,48 @@ class StatusDisplay (Entity) :
 		self._layer = 10
 		self._size = size
 
+		#for performance
+		self._needs_redraw = True
+
+		self._health = None
+		self._max_health = None
+		self._name = None
+		self._weapon_str = None
+
 	def get_client (self) :
 		return self._client
 	def set_client (self, client) :
 		self._client = client
 
 	def update (self) :
+		assert (self._client != None)
+
+		old_health = self._health
+		old_max_health = self._max_health
+		old_name = self._name
+		old_weapon_str = self._weapon_str
+
+		self._health = self._client.get_health ()
+		self._max_health = self._client.get_max_health ()
+		self._name = self._client.get_name ()
+		self._weapon_str = self._client.get_weapon_str ()
+
+		self._needs_redraw = (
+			self._health != old_health or
+			self._max_health != old_max_health or
+			self._name != old_name or
+			self._weapon_str != old_weapon_str)
+
 		Entity.update (self)
 
-	def update_image (self) :
-		self.image = pygame.Surface (self._size)
-		self.image.set_colorkey ((255,255,255))
-		self.image.fill ((255,255,255))
 
-		if self._client != None :
+	def update_image (self) :
+		if self._needs_redraw :
+			if self.image == None :
+				self.image = pygame.Surface (self._size)
+				self.image.set_colorkey ((255,255,255))
+
+			self.image.fill ((255,255,255))
 			self._add_health_bar ()
 			self._add_name ()
 			self._add_weapon ()
@@ -44,7 +72,7 @@ class StatusDisplay (Entity) :
 		pygame.draw.rect (self.image, (0,0,0), health_bar_rect, 1)
 
 		#current health
-		health_percent = 1.0 * self._client.get_health () / self._client.get_max_health ()
+		health_percent = 1.0 * self._health / self._max_health
 		cur_health_bar_rect = health_bar_rect
 		cur_health_bar_rect.width *= health_percent
 		pygame.draw.rect (self.image, (255,0,0), health_bar_rect)
@@ -52,7 +80,7 @@ class StatusDisplay (Entity) :
 
 	def _add_name (self) :
 		font = pygame.font.Font (None, 20)
-		text = font.render (self._client.get_name (), False, (100,100,100), (255,255,255)).convert ()
+		text = font.render (self._name, False, (100,100,100), (255,255,255)).convert ()
 		textpos = text.get_rect ()
 		textpos.centerx = self.image.get_rect().centerx
 		textpos.top = self.image.get_rect().top
@@ -60,7 +88,7 @@ class StatusDisplay (Entity) :
 
 	def _add_weapon (self) :
 		font = pygame.font.Font (None, 20)
-		text = font.render (self._client.get_weapon_str (), False, (100,100,100), (255,255,255)).convert ()
+		text = font.render (self._weapon_str, False, (100,100,100), (255,255,255)).convert ()
 		textpos = text.get_rect ()
 		textpos.centerx = self.image.get_rect().centerx
 		textpos.centery = self.image.get_rect().centery
